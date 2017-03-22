620930dd5dfa5f1432506264cb4c6cee183dcb76
ianhay <sirianhay@gmail.com>
2017-03-22 15:08:49 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bIM90eOh-wMJAVZ6s4B9y09mYucpIalzz9RB2l8M1T4/pubhtml';
